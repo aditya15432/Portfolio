071dddf0713abda2acfff5ec3c308d93cc8c7782
Manoj1999-kumar <manoj.hemanth143@gmail.com>
2023-10-10 15:52:55 +0530

    change: local set for project and removed unwanted folders.

--- user_portfolio/src/main.js
+++ user_portfolio/src/main.js
@@ -1,14 +1,25 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from "pinia";
+import { Quasar } from "quasar";
+import Vue3Santitize from "vue-3-Sanitize";
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
+const allowedTagesAndAttributes = {
+    allowedTags : ["a", "img", "br", "span", "u", "p", "li", "strong"],
+    allowedAttributes : {
+        a: ["href", "class", "download"],
+        span: ["class", "id"],
+    },
+};
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+    plugins: {}, 
+});
+app.use(Vue3Santitize , allowedTagesAndAttributes);
 
 app.mount('#app')
---
+++ user_portfolio/src/views/ResumeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HomeTab from "../components/HomeTab.vue"
+</script>
+
+<template>
+  <main>
+    <home-tab/>
+  </main>
+</template>
