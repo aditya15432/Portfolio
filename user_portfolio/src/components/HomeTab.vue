2f6675cb4167e1c0cb738b0685f7647f7f9ebbf2
Aditya <71317171+aditya15432@users.noreply.github.com>
2024-09-09 13:22:14 -0500

    change: code testing

--- user_portfolio/src/components/HomeTab.vue
+++ user_portfolio/src/components/HomeTab.vue
@@ -1,6 +1,15 @@
 <template >
     <div>
+      <q-card>
       <q-btn color="purple" text-color="black" label="Standard" />
+      <q-separator inset />
       <q-btn color="secondary" label="Secondary" />
+    </q-card>
     </div>
 </template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
